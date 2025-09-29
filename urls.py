5814884fa02ede8f2ca1cde1eea92bffab288866
Melaku Garsamo <melaku.garsamo@gmail.com>
2025-09-29 15:37:21 -0500

    Revert OAuth configuration back to working state
    - Revert to include('listings.urls') for /oauth/ endpoint
    - This was working before the bad request error

--- urls.py
+++ urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from listings.views import google_oauth_endpoint
 
 urlpatterns = [
     path('admin/', admin.site.urls),
